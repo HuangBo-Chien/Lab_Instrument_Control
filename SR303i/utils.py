b2af71c5a49cb598e27d93ca8102e750c6a47093
HuangBo-Chien <abc29298727@gmail.com>
2021-05-30 23:58:51 +0800

    add set temp and read temp

--- SR303i/utils.py
+++ SR303i/utils.py
@@ -8,26 +8,15 @@ class Andor_Spectrometer:
         self.atshamrock = cdll.LoadLibrary(self.path + "atshamrock.dll")
         self.ShamrockCIF = cdll.LoadLibrary(self.path + "ShamrockCIF.dll")
         self.ATSHAMROCKCS = cdll.LoadLibrary(self.path + "ATSHAMROCKCS.dll")
-        self.nod = c_int(100) # 先隨意初始化一個數字
+        
         self._DRV_SUCC()
-        self.SN = create_string_buffer(64) # seriaal number，暫時給一個64bit的string buffer
-        self.FL = c_float(0.0) # focal length
-        self.AD = c_float(0.0) # Angular Deviation
-        self.FT = c_float(0.0) #  
         self.DEV_Info()
-        self.GP = c_int(100) # Grating Present
-        self.Turret = c_int(5)
-        self.NG = c_int(100) # No Grating
-        self.G = c_int(100) # Grating
-        self.DO = c_int(100) # Detector offset
-        self.lines = c_float(0.0)
-        self.blaze = create_string_buffer(64)
-        self.home = c_int(100)
-        self.offset = c_int(100)
-        self.GO = c_int(100) # Grating offset
         self.read_set()
 
     def _DRV_SUCC(self):
+    
+        self.nod = c_int(100) # 先隨意初始化一個數字
+    
         status = self.ShamrockCIF.ShamrockInitialize(self.path + "detector.ini")
         if status == 20202:
             self.ShamrockCIF.ShamrockGetNumberDevices.argtypes = [POINTER(c_int)] # 用傳參考的方式，傳一個整數
@@ -43,6 +32,12 @@ class Andor_Spectrometer:
             return False
 
     def DEV_Info(self):
+    
+        self.SN = create_string_buffer(64) # seriaal number，暫時給一個64bit的string buffer
+        self.FL = c_float(0.0) # focal length
+        self.AD = c_float(0.0) # Angular Deviation
+        self.FT = c_float(0.0) #  
+    
         self.ShamrockCIF.ShamrockGetSerialNumber.argtypes = [c_int, c_char_p] # 傳一個整數跟一個char *
         status = self.ShamrockCIF.ShamrockGetSerialNumber(0, self.SN)
         if status == 20202:
@@ -58,6 +53,18 @@ class Andor_Spectrometer:
             return False
 
     def read_set(self):
+    
+        self.GP = c_int(100) # Grating Present
+        self.Turret = c_int(5)
+        self.NG = c_int(100) # No Grating
+        self.G = c_int(100) # Grating
+        self.DO = c_int(100) # Detector offset
+        self.lines = c_float(0.0)
+        self.blaze = create_string_buffer(64)
+        self.home = c_int(100)
+        self.offset = c_int(100)
+        self.GO = c_int(100) # Grating offset
+    
         self.ShamrockCIF.ShamrockGratingIsPresent.argtypes = [c_int, POINTER(c_int)]
         self.ShamrockCIF.ShamrockGetTurret.argtypes = [c_int, POINTER(c_int)]
         self.ShamrockCIF.ShamrockGetNumberGratings.argtypes = [c_int, POINTER(c_int)]
@@ -87,8 +94,26 @@ class Andor_Spectrometer:
           %(self.GP.value, self.Turret.value, self.NG.value, self.G.value, self.DO.value,
             self.GO.value, self.lines.value, self.blaze.value, self.home.value, self.offset.value)
         )
-            
-if __name__ == "__main__":
-
-    SR303i = Andor_Spectrometer()
+          
+    def setT(self, temp):
+        c_temp = c_int(temp)
+        self.ATMCD64D.SetTemperature(c_temp)
+        self.ATMCD64D.CoolerON()
+    
+    def readT(self):
+        current_temp = c_int(0)
+        self.ATMCD64D.GetTemperature.argtypes = [POINTER(c_int)]
+        status = self.ATMCD64D.GetTemperature(byref(current_temp)) # 如果status == 20036表示溫度穩定
+        return status, current_temp.value
         
+if __name__ == "__main__":
+    import time
+    SR303i = Andor_Spectrometer()
+    SR303i.setT(-40)
+    while True:
+        s, t = SR303i.readT()
+        print("Temp = %d" %(t))
+        if s == 20036:
+            print("Temp is stable")
+            break
+        time.sleep(1)
