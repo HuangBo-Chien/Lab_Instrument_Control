95e1fbb25cdc3bd733a34bfc2c3a8d5c33127016
HuangBo-Chien <abc29298727@gmail.com>
2021-12-04 01:33:21 +0800

    some functions and setting of model 2182

---
+++ Model_2182_Utilities.py
@@ -0,0 +1,67 @@
+import pyvisa as pv
+import time
+
+rm = pv.ResourceManager()
+
+class Model_2182:
+
+    def __init__(self, GPIB_Addr:str) -> None:
+        self.Addr = GPIB_Addr
+        self.Identity = rm.open_resource(GPIB_Addr)
+    
+    def Start(self) -> None:
+        self.Identity.write("*RST")
+    
+    def Select_Volt_Range(self, Range:float = 10) -> None:
+        if Range < 0:
+            Range = 0
+        elif Range > 120:
+            Range = 120
+        self.Identity.write(f":SENS:RANG {Range}")
+    
+    def Display(self, Enable:bool = True) -> None:
+        if Enable:
+            self.Identity.write(":DISP:ENAB ON")
+        else:
+            self.Identity.write(":DISP:ENAB OFF")
+    
+    def Sense_Volt(self, Avg_times:int =  1) -> float:
+        if Avg_times < 1:
+            Avg_times = 1
+        val = 0
+        for _ in range(Avg_times):
+            val += self.Identity.query(":SENS:DATA")
+            time.sleep(0.1) # sleep for 100 ms
+        val /= Avg_times
+        return val
+    
+    def Select_PLC(self, PLC:float = 1) -> None:
+        '''
+        In Taiwan, our electricity is 60 Hz
+        '''
+        if PLC < 0.01:
+            PLC = 0.01
+        elif PLC > 60:
+            PLC = 60
+        self.Identity.write(f":SENS:NPLC {PLC}")
+    
+    def Filter_Setting(self, Enable:bool = True, Type:int = 0, Count:int = 10, Window:float = 0.01) -> None:
+        if Enable:
+            self.Identity.write(f":SENS:WIND {Window}")
+            if Type == 0:
+                self.Identity.write(":SENS:TCON MOV") # Moving window
+            else:
+                self.Identity.write(":SENS:TCON REP") # Repeating window
+            if Count < 1:
+                Count = 1
+            elif Count > 100:
+                Count = 100
+            self.Identity.write(f":SENS:COUN {Count}")
+            self.Identity.write(":SENS:DFIL ON")
+        else:
+            self.Identity.write(":SENS:DFIL OFF")
+        
+if __name__ == "__main__":
+    my_Model_2182 = Model_2182("GPIB0::7::INSTR")
+
+    
