76a2a2405a3f5c85485b2df40fed54d4cacde0db
HBC <abc29298727@gmail.com>
2022-01-20 22:45:47 +0800

    reconstruct pulsedelta mode

--- Model_6221_Utilities.py
+++ Model_6221_Utilities.py
@@ -1,3 +1,4 @@
+from turtle import width
 import pyvisa as pv
 import time
 import numpy as np
@@ -9,30 +10,52 @@ class Model_6221:
     def __init__(self, GPIB_Addr:str) -> None:
         self.Addr = GPIB_Addr
         self.Identity = rm.open_resource(GPIB_Addr)
+        self.Reset()
     
     def Reset(self) -> None:
         self.Identity.write("*RST")
 
-    class Model_2182_Setting:
+    def Check_2812_Connection(self, Mode:str) -> bool:
+        '''
+        Mode can only be DEL, PDEL, DCON
+        '''
+        Modes = ("DEL", "PDEL", "DCON")
+        if Mode in Modes:
+            q = self.Identity.query(f":SOUR:{Mode}:NVPR?") # check if Model 2182 is correctly connected with Nodel 6221
+            return (q == 1) # 1 = yes, 0 = no
+        else:
+            return False
+    
+    def Send_Command_to_2182(self, Commands:list) -> None:
+        '''
+        send commands from 6221 to 2182
+        '''
+        prefix = "SYST:COMM:SER "
+        for command in Commands:
+            self.Identity.write(f"{prefix}\"{command}\"")
+    
+    def Set_Compliance(self, Comp:int = 105) -> None:
+        if 1 <= Comp <= 105:
+            self.Identity.write(f"SOUR:CURR:COMP {Comp}")
 
-        def __init__(self, RNG:float = 0.1, NPLC:int = 1) -> None:
-            self.RNG = RNG
-            self.NPLC = NPLC
-
-        def Check_Connection(self, Mode:str) -> bool:
-            '''
-            Mode can only be DEL, PDEL, DCON
-            '''
-            Modes = ("DEL", "PDEL", "DCON")
-            if Mode in Modes:
-                q = self.Identity.query(f":SOUR:{Mode}:NVPR?") # check if Model 2182 is correctly connected with Nodel 6221
-                return (q == 1) # 1 = yes, 0 = no
-            else:
-                return False
+    def PulseDelta_Mode(self) -> np.array:
+        PD = self.Pulsedelta
+        commands = PD.Generate_Command()
+        for command in commands:
+            self.Identity.write(command)
         
-        def Send_Command(self, Command:str) -> None:
-            prefix = "SYST:COMM:SER "
-            # self.
+        self.Identity.write(":SOUR:PDEL:ARM")
+        self.Identity.write(":INIT:IMM") # start the measurement
+        count_num = 0
+        while True:
+            event_response = self.Identity.query(":STAT:OPER:EVEN?")
+            if (event_response & 1024) >> 10 and not (event_response & 64) >> 6:
+                break
+            if (event_response & 32) >> 5:
+                count_num += 1
+                print(count_num)
+        self.Identity.write(":SOUR:SWE:ABOR")
+        return np.reshape(np.array(map(np.float64, self.Identity.query(":TRAC:DATA?").split(","))), (-1, 2))
 
     class Pulsedelta:
         """
@@ -40,26 +63,81 @@ class Model_6221:
         Unit of params related to current are uA.
         Unit of delay time and pulse width are us.
         Unit of interval is power line cycle (PLC). e.g. 60Hz ---> 1 PLC = 1 / 60 s ~ 16.667 ms
+        
+        If Sweep mode is on, then I_high and I_low are replaced with I_start and I_stop in the sweep mode.
+        CountNUm in the Pulse Delta mode is also replaced with CountNum in the Sweep mode.
         """
-        def __init__(self, Sweep:bool = False) -> None:
-            if Sweep:
-                self.Sweep_Pulse()
-            else:
-                self.Fixed_Pulse()
+        def __init__(self) -> None:
+            self.PD = {}
+            self.PD["HIGH"] = 100.0
+            self.PD["LOW"] = 0.0
+            self.PD["WIDT"] = 100
+            self.PD["SDEL"] = 100
+            self.PD["COUN"] = 1
+            self.PD["RANG"] = "Best"
+            self.PD["INT"] = 5
+            self.PD["LME"] = 2
+            self.PD["SWE"] = False
+            # sweep setting for the sweep mode
+            self.SWE = {}
+            self.SWE["SPAC"] = 1
+            self.SWE["POIN"] = 1
+            self.SWE["RANG"] = "Best"
+            self.SWE["COUN"] = 1
+            # current setting in tje sweep mode
+            self.CURR = {}
+            self.CURR["STAR"] = 10.0
+            self.CURR["STOP"] = 0.0
+            self.CURR["COMP"] = 105
+        
+        def Set_I_High(self, I_high:float) -> None:
+            self.PD["HIGH"] = I_high
+        
+        def Set_I_Low(self, I_low:float) -> None:
+            self.PD["LOW"] = I_low
+        
+        def Set_Pulse_Width(self, Width:int) -> None:
+            self.PD["WIDT"] = Width
+        
+        def Set_Source_Delay(self, Delay:int) -> None:
+            self.PD["SDEL"] = Delay
+        
+        def Set_Count_Num(self, Number:int) -> None:
+            self.PD["COUN"] = Number
+        
+        def Set_Source_Range(self, Mode:str) -> None:
+            self.PD["RANG"] = Mode
+        
+        def Set_Interval(self, Interval:int) -> None:
+            self.PD["INT"] = Interval
+        
+        def Set_Low_Measurement(self, Mode:int) -> None:
+            self.PD["LME"] = Mode
+        
+        def Set_Sweep(self, Mode:bool) -> None:
+            self.PD["SWE"] = Mode
+        
+        def Set_CURR_Compliance(self, Comp:int) -> None:
+            self.CURR["COMP"] = Comp
+        
+        def Set_CURR_Start(self, I:float) -> None:
+            self.CURR["STAR"] = I
 
-        def Fixed_Pulse(self, I_high:np.float64 = np.float64(100.0), I_low:np.float64 = np.float64(100.0), Width:np.float64 = np.float64(100.0), Src_delay:np.float64 = np.float64(100), CountNum:str = "1", SrcRng:str = "Best", Interval:np.int64 = 5, Low_Meas:np.int64 = 2) -> None:
-            self.I_high = np.around(I_high, decimals = 2)
-            self.I_low = np.around(I_low, decimals = 2)
-            self.Width = np.around(Width, decimals = 2)
-            self.Src_delay = np.around(Src_delay, decimals = 2)
-            self.CountNum = CountNum
-            self.SrcRng = SrcRng
-            self.Interval = Interval
-            self.Low_Meas = Low_Meas
+        def Set_CURR_Stop(self, I:float) -> None:
+            self.CURR["STOP"] = I
         
-        def Sweep_Pulse(self) -> None:
-            pass
+        def Set_Sweep_Cycle(self, Cycle:int) -> None:
+            self.SWE["COUN"] = 1
+
+        def Set_Sweep_Range(self, Range:str) -> None:
+            self.SWE["RANG"] = Range
         
+        def Set_Sweep_Point(self, Pt:int) -> None:
+            self.SWE["POIN"] = Pt
+        
+        def Set_Sweep_Pattern(self, Mode:int) -> None:
+            self.SWE["SPAC"] = Mode
+
         def Show_Setting(self):
             print(f"\
             ********** Pulse Delta Mode Setting **********\
@@ -71,6 +149,20 @@ class Model_6221:
             Source Range setting = {self.SrcRng} \n\
             Interval = {self.Interval} PLC \n\
             **********************************************")
+        
+        def Generate_Command(self) -> list:
+            Commands = []
+            prefix = "SOUR:PDEL:"
+            for key, val in self.PD.items():
+                Commands.append(f"{prefix}{key} {val}")
+            if self.PD["SWE"] == True:
+                prefix = "SOUR:SWE:"
+                for key, val in self.SWE.items():
+                    Commands.append(f"{prefix}{key} {val}")
+                prefix = "SOUR:CURR:"
+                for key, val in self.CURR.items():
+                    Commands.append(f"{prefix}{key} {val}")
+            return Commands
 
     class Sweep:
 
@@ -120,23 +212,10 @@ class Model_6221:
         if Mode == "PulseDelta":
             pass
 
-    def Communication_with_2182(self, RNG:float = 1, NPLC:int = 2):
-        pass
-    
-    def PulseDelta_Measure(self) -> list:
-        self.Identity.write(":SOUR:PDEL:ARM")
-        self.Identity.write(":INIT:IMM") # start the measurement
-        count_num = 0
-        while True:
-            event_response = self.Identity.query(":STAT:OPER:EVEN?")
-            if (event_response & 1024) >> 10 and not (event_response & 64) >> 6:
-                break
-            if (event_response & 32) >> 5:
-                count_num += 1
-                print(count_num)
-        self.Identity.write(":SOUR:SWE:ABOR")
-        return self.Identity.query(":TRAC:DATA?")
-
-
 if __name__ == "__main__":
     my_Model_6221 = Model_6221
+    # check model 2182 connection
+    # set model 2182 volt range and number of plc
+    # set model 6221 to pulse delta mode
+    # arm the mode
+    # start the measurement
