7dcff03a19bd7eef5c5fefb81a7649a3453b8ea7
HBC <abc29298727@gmail.com>
2022-03-18 11:53:18 +0800

    fix bugs

--- Kepco_BOP_Utilities.py
+++ Kepco_BOP_Utilities.py
@@ -12,6 +12,7 @@ class Kepco_BOP:
 
     def Reset(self) -> None:
         self.Instr.write("*RST")
+        self.Clear()
     
     def Clear(self) -> None:
         self.Instr.clear()
@@ -22,6 +23,7 @@ class Kepco_BOP:
         '''
         if Mode == "VOLT" or Mode == "CURR":
             self.Instr.write(f"FUNC:MODE {Mode}")
+        self.Clear()
     
     def Power_Output(self, Switch:bool) -> None:
         '''
@@ -31,6 +33,7 @@ class Kepco_BOP:
             self.Instr.write("OUTP ON")
         else:
             self.Instr.write("OUTP OFF")
+        self.Clear()
     
     def Set_Current_Output(self, Current:float) -> None:
         '''
@@ -38,6 +41,7 @@ class Kepco_BOP:
         this can only be used when the output mode is current mode
         '''
         self.Instr.write(f"CURR {Current}")
+        self.Clear()
 
     def Set_Voltage_Limit(self, Voltage:float) -> None:
         '''
@@ -45,6 +49,7 @@ class Kepco_BOP:
         this can only be used when the output mode is current mode
         '''
         self.Instr.write(f"VOLT {Voltage}")
+        self.Clear()
     
     def Set_Voltage_Output(self, Voltage:float) -> None:
         '''
@@ -52,6 +57,7 @@ class Kepco_BOP:
         this can only be used when the output mode is voltage mode
         '''
         self.Instr.write(f"VOLT {Voltage}")
+        self.Clear()
     
     def Set_Current_Limit(self, Current:float) -> None:
         '''
@@ -61,4 +67,9 @@ class Kepco_BOP:
         self.Instr.write(f"CURR {Current}")
 
 if __name__ == "__main__":
-    pass
+    BOP_36_12 = Kepco_BOP("GPIB::6::INSTR")
+    BOP_36_12.Power_Output(Switch = True)
+    BOP_36_12.Output_Mode("CURR")
+    BOP_36_12.Set_Voltage_Limit(Voltage = 10)
+    BOP_36_12.Set_Current_Output(Current = 1)
+    
