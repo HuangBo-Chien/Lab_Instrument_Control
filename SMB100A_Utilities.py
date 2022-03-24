7b1b667254a0fa01a1a2e41086accdb92361ef88
HuangBo-Chien <abc29298727@gmail.com>
2022-03-25 02:10:35 +0800

    add comments to describe more params' details

--- SMB100A_Utilities.py
+++ SMB100A_Utilities.py
@@ -17,11 +17,13 @@ class SMB100A:
     
     def Set_CW_Freq(self, Freq:float, Unit:int = 3) -> None:
         '''
-        Set the freq of continuous wave
-        Unit == 0 ---> Hz
-        Unit == 1 ---> kHz
-        Unit == 2 ---> MHz
-        Unit == 3 ---> GHz
+        Set the freq of continuous wave.
+
+        Parameters
+        ==========
+            Freq can be any positive values as long as the device can achieve.
+
+            Unit == 0/1/2/3 ---> Hz/kHz/MHz/GHz. Default is GHz.
         '''
         if Unit < 1 or Unit > 3:
             return
@@ -29,11 +31,13 @@ class SMB100A:
     
     def Set_Sweep_by_Center_Span(self, Center:float, Span:float, Unit:int = 3) -> None:
         '''
-        Set freq sweeping by specifing center freq and freq span
-        Unit == 0 ---> Hz
-        Unit == 1 ---> kHz
-        Unit == 2 ---> MHz
-        Unit == 3 ---> GHz
+        Set freq sweeping by specifing center freq and freq span.
+
+        Parameters
+        ==========
+            Center and Span can be any positive values as long as the device can achieve.
+
+            Unit == 0/1/2/3 ---> Hz/kHz/MHz/GHz. Default is GHz.
         '''
         if Unit < 1 or Unit > 3:
             return
@@ -42,11 +46,13 @@ class SMB100A:
     
     def Set_Sweep_by_Start_Stop(self, Start:float, Stop:float, Unit:int = 3) -> None:
         '''
-        Set freq sweeping by specifing freq start and freq stop
-        Unit == 0 ---> Hz
-        Unit == 1 ---> kHz
-        Unit == 2 ---> MHz
-        Unit == 3 ---> GHz
+        Set freq sweeping by specifing freq start and freq stop.
+
+        Parameters
+        ==========
+            Start and Stop can be any positive values as long as the device can achieve. Stop > Start.
+
+            Unit == 0/1/2/3 ---> Hz/kHz/MHz/GHz. Default is GHz.
         '''
         if Unit < 1 or Unit > 3:
             return
@@ -58,16 +64,23 @@ class SMB100A:
     
     def Set_Output_Power(self, Power:float) -> None:
         '''
-        Set output power (dBm)
+        Set output power (dBm).
+        
+        Parameter
+        ========
+            Power can be any value as long as the device can really achieve.
         '''
         self.Instr.write(f"SOUR:POW:LEV:IMM:AMPL {Power}")
     
-    def Set_Output_State(self, State:bool) -> None:
+    def Set_Output_State(self, Switch:bool) -> None:
         '''
-        Switch on/off the output
-        State can be either Ture or False to indicate On or OFF
+        Switch on/off the output.
+
+        Parameter
+        ==========
+            Switch == True/False ---> Turn on/off the output.
         '''
-        if State:
+        if Switch:
             self.Instr.write("OUTP:STAT ON")
         else:
             self.Instr.write("OUTP:STAT OFF")
@@ -76,9 +89,9 @@ if __name__ == "__main__":
     mySMB100A = SMB100A("GPIB::28::INSTR")
     mySMB100A.Set_CW_Freq(Freq = 10) # set freq to 10 GHz
     mySMB100A.Set_Output_Power(Power = 15) # set power to 15 dBm
-    mySMB100A.Set_Output_State(State = True)
+    mySMB100A.Set_Output_State(Switch = True)
     
     from time import sleep
     sleep(10)
-    mySMB100A.Set_Output_State(State = False)
+    mySMB100A.Set_Output_State(Switch = False)
 
